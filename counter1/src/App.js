e8fc7b4a0057b3ca5802663a9623c1203ef864f0
Guilherme Chadt <chadt7oficial@gmail.com>
2021-07-23 18:30:36 -0300

    Counter App

---
+++ counter1/src/App.js
@@ -0,0 +1,12 @@
+import { Card } from "./Components/Card/Card";
+
+
+function App() {
+  return (
+    <>
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ counter1/src/Components/Card/Card.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Button from "../Button/Button"
+
+
+export const Card = () => {
+
+    const [value, setValue] = useState(1);
+
+    function Adicionar() {
+        setValue(value + 1)
+    }
+
+    function Subtrair() {
+        setValue(value - 1)
+    }
+
+
+
+    return (
+        <>
+            <h1>Counter App</h1>
+            <hr></hr>
+            <div className="card">
+                <div className="card-header">
+                    <h1>{value}</h1>
+                </div>
+                <div className="card-body">
+                    <Button
+                        className="btn btn-primary"
+                        onClick={Adicionar}
+                    >
+
+                        Adicionar
+                    </Button>
+                    <Button
+                        className="btn btn-danger"
+                        onClick={Subtrair}
+                    >
+
+                        Subtrair
+                    </Button>
+                </div>
+            </div>
+        </>
+    )
+}
